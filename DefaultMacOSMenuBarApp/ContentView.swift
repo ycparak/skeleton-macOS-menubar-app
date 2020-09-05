81b860bb358d16c45da6f79264b6c18045fbf5fc
Yusuf Parak <yusuf@thinkst.com>
2020-09-05 16:12:45 +0200

    Initial Commit

---
+++ DefaultMacOSMenuBarApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DefaultMacOSMenuBarApp
+//
+//  Created by Yusuf Parak on 2020/09/05.
+//  Copyright © 2020 Yusuf Parak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
